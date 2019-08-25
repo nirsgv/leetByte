b1282b93eada5e89d4cca37dc35c646f616b696d
nir.segev <nir.segev@ladbrokescoral.com>
2019-08-25 15:23:21 +0300

    traversing trees bfs and dfs methods added

--- dataStructures/BinarySearchTree/BinarySearchTree.js
+++ dataStructures/BinarySearchTree/BinarySearchTree.js
@@ -11,12 +11,12 @@ class BinarySearchTree {
         this.root = null;
     }
 
-
     /**
      *
      * @param {Number} - val - a number value to insert in a sorted manner to the binary search tree.
      * @returns {*}
      */
+
     insert(val) {
         let newNode = new Node(val);
         if (!this.root) return this.root = newNode;
@@ -39,6 +39,7 @@ class BinarySearchTree {
      * @param val - {Number} - a number value to search for in the binary search tree.
      * @returns {*} - returns the found node, or null if not found
      */
+
     find(val) {
         if (!this.root) return undefined;
         let node = this.root;
@@ -52,17 +53,84 @@ class BinarySearchTree {
             }
         })(val, node);
     }
+
+    /**
+     * breadh first search = we want to visit every node on the same level, visit every sibling node before
+     * we visit a child
+     * @returns {Array}
+     */
+
+    bfs() {
+        let node = this.root,
+            data = [],
+            queue = [];
+        queue.push(node);
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        return data;
+    }
+
+
+    /**
+     * depth first search - preorder
+     * persue all nodes down vertically before visiting sibling nodes horizontally
+     */
+    dfsPreOrder() {
+        let visited = [];
+        (function preOrder(node) {
+            visited.push(node);
+            node.left && preOrder(node.left);
+            node.right && preOrder(node.right);
+        })(this.root);
+        return visited;
+    }
+
+    /**
+     * depth first search - postOrder
+     * persue all nodes down vertically before visiting sibling nodes horizontally
+     */
+    dfsPostOrder() {
+        let visited = [];
+        (function preOrder(node) {
+            node.left && preOrder(node.left);
+            node.right && preOrder(node.right);
+            visited.push(node);
+        })(this.root);
+        return visited;
+    }
+
+    /**
+     * depth first search - postOrder
+     * persue all nodes down vertically before visiting sibling nodes horizontally
+     * inOrder - if we look at the data we get back, it is in-order, we get all the nodes in the tree
+     * in their underlying order
+     */
+    dfsInOrder() {
+        let visited = [];
+        (function inOrder(node) {
+            node.left && inOrder(node.left);
+            visited.push(node);
+            node.right && inOrder(node.right);
+        })(this.root);
+        return visited;
+    }
 }
 
 let bst = new BinarySearchTree();
-console.log(bst.insert(100));
 console.log(bst.insert(10));
-console.log(bst.insert(110));
-console.log(bst.insert(11));
-console.log(bst.insert(8));
-console.log(bst.insert(9));
-console.log(bst.insert(7));
+console.log(bst.insert(6));
 console.log(bst.insert(15));
+console.log(bst.insert(3));
+console.log(bst.insert(8));
+console.log(bst.insert(20));
 console.log(bst.find(11));
 console.log(bst.find(12));
+console.log(bst.bfs());
+console.log(bst.dfsPreOrder());
+console.log(bst.dfsPostOrder());
+console.log(bst.dfsInOrder());
 console.log(bst);
